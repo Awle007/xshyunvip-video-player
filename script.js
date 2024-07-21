560c75a5c1c3d590d88fec5294a70ee2685114c9
Awle_ <141636413+Awle007@users.noreply.github.com>
2024-07-22 01:33:49 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,5 @@
 let form = document.querySelector('form')
 let returnBtn = document.querySelector('.card-back .return')
-let timerBtn = document.querySelector('.card-back .timer')
-let timer
-let setTimer = document.querySelector('.set-timer')
-let timer30 = document.querySelector('.set-timer-30')
-let timer60 = document.querySelector('.set-timer-60')
-let timer90 = document.querySelector('.set-timer-90')
 let cancelBtn = document.querySelector('.cancel-btn')
 let delay
 
@@ -34,32 +28,6 @@ form.addEventListener('submit', (e) => {
     mediaInput.value = ''
     window.clearTimeout(delay)
   })
-})
-
-timerBtn.addEventListener('click', function () {
-  console.log('clicked')
-  setTimer.classList.toggle('show-set-timer')
-})
-
-timer30.addEventListener('click', (e) => {
-  setTimer.classList.remove('show-set-timer')
-  timer = window.setTimeout(function turnToFront() {
-    returnBtn.click()
-  }, 1800000)
-})
-
-timer60.addEventListener('click', (e) => {
-  setTimer.classList.remove('show-set-timer')
-  timer = window.setTimeout(function turnToFront() {
-    returnBtn.click()
-  }, 3600000)
-})
-
-timer90.addEventListener('click', (e) => {
-  setTimer.classList.remove('show-set-timer')
-  timer = window.setTimeout(function turnToFront() {
-    returnBtn.click()
-  }, 5400000)
 })
 
 cancelBtn.addEventListener('click', (e) => {
